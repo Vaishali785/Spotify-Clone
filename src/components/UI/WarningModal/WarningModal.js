446230dc3bab6859b5fd8118978524a5c96e493f
Vaishali <aggarwal.vaishali785@gmail.com>
2024-03-28 23:12:00 +0530

    added dummy credentials modal

--- src/components/UI/WarningModal/WarningModal.js
+++ src/components/UI/WarningModal/WarningModal.js
@@ -8,17 +8,17 @@ const WarningModal = ({ showWarning, clickHandler }) => {
                 <span className={classes["account-warning-modal__close"]} onClick={() => showWarning(false)}>
                     <CloseRoundedIcon />
                 </span>
-                <h3 className={classes["account-warning-modal__heading"]}>Important</h3>
-                <li>
+                <h3 className={classes["account-warning-modal__heading"]}>Dummy Credentials</h3>
+                {/* <li>
                     <span>Login with personal Spotify account: Changes made here affect your personal account.</span>
                 </li>
-                <li>
-                    <span>Use dummy credentials to avoid personal account changes.</span>
-                    <div className={classes["credentials"]}>
-                        <div >Email: mydemomail78@gmail.com</div>
-                        <div >Password: test#12345</div>
-                    </div>
-                </li>
+                <li> */}
+                <span>During the development phase, the Spotify API currently doesn't allow users to access their personal accounts. For testing purposes, dummy credentials are given below: </span>
+                <div className={classes["credentials"]}>
+                    <div >Email: mydemomail78@gmail.com</div>
+                    <div >Password: test#12345</div>
+                </div>
+                {/* </li> */}
                 <button className={classes['login-btn']} onClick={clickHandler}>Login</button>
             </ul>
         </div>
