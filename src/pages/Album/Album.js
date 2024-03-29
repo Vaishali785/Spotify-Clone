3af287177be76920e56ec25afacbc3f3a9b2ad9d
Vaishali <aggarwal.vaishali785@gmail.com>
2024-03-29 23:03:15 +0530

    fixed issue of images not showing in footer for album pages

--- src/pages/Album/Album.js
+++ src/pages/Album/Album.js
@@ -7,13 +7,14 @@ import AlbumSongsList from '../../components/Modules/Albums/AlbumItem__Songs/Son
 const Album = () => {
     const { albumId } = useParams();
     const [colors, setColors] = useState();
+    const [trackImg, setTrackImg] = useState();
 
     return (
         <div className={classes['playlist-page']}>
-            <AlbumItemHeader albumId={albumId} setColors={setColors} colors={colors} />
+            <AlbumItemHeader albumId={albumId} setTrackImg={setTrackImg} setColors={setColors} colors={colors} />
 
             {/* <SongsTitleBar /> */}
-            <AlbumSongsList albumId={albumId} colors={colors} />
+            <AlbumSongsList albumId={albumId} colors={colors} trackImg={trackImg} />
         </div>
     )
 
