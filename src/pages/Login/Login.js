d5f124ed0db6326458fd7b96e82b00a60256de9a
Vaishali <aggarwal.vaishali785@gmail.com>
2024-03-29 23:04:54 +0530

    added dummy credentials modal on login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from "react";
-// import { login_url } from '../../utilities/auth/LoginScopes';
-import { authUrl, params } from "../../utilities/auth/GetTokenNew";
+import React, { useState } from "react";
+import { params } from "../../utilities/authParams/GetToken";
 import classes from './Login.module.css';
 import IMAGES from "../../assets/images";
 import WarningModal from "../../components/UI/WarningModal/WarningModal";
 
 
 export default function Login() {
-    // const _token = useGenerateToken();
-    const [warning, showWarning] = useState();
-
-    // console.log("login")
+    const [warning, showWarning] = useState(false);
     const submitHandler = (e) => {
-        showWarning(false);
-        console.log("hey")
+        // showWarning(false);
+        const currentUrl = window.location.href;
         e.preventDefault();
         const authUrl = new URL('https://accounts.spotify.com/authorize');
+        params.redirect_uri = currentUrl;
         const urlParams = params;
         authUrl.search = new URLSearchParams(urlParams).toString();
         window.location.href = authUrl.toString();
-        // console.log("hey")
     }
+
     return (
         <div className={classes.login}>
             <img src={IMAGES.spotifyLogoImg} alt="Spotify Logo" />
-            <button className={classes['start-btn']} onClick={() => showWarning(true)}>Start App</button>
+            <button className={classes['start-btn']} onClick={() => showWarning(prev => !prev)}>Start App</button>
+            {/* {params} */}
+            {/* <p className={classes["warning"]}><b>Caution :</b> Changes made here affect your personal account.</p> */}
             {warning && <WarningModal showWarning={showWarning} clickHandler={submitHandler} />}
         </div>
     )
