951289ebb73b0ac0d8a3e7487384bb6d1d35802a
Vaishali <aggarwal.vaishali785@gmail.com>
2024-04-23 21:29:19 +0530

    page modules: more options modal - users playlist

---
+++ src/components/Modules/Playlist/PlaylistCard/PlaylistCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import classes from './PlaylistCard.module.css';
+import HoverableCard from '../../../UI/Card/HoverableCard/HoverableCard';
+
+const PlaylistCard = ({ item }) => {
+    const removeHTMLFromContent = (content) => {
+        const tagRegExp = new RegExp('<\s*[^>]*>', 'g')
+        return content.replace(tagRegExp, '');
+    }
+    return (
+        <HoverableCard link={`/playlist/${item.id}`}>
+            <img src={item.images[0].url} alt="" className={`${classes['item-img']}`} />
+            <div className={`${classes["item-details"]}`}>
+                <div className={`${classes["item-details__name"]} truncate-name title`}>
+                    {item.name}
+                </div>
+                <div className={`${classes["item-details__description"]} text-no-hover truncate-description`}>{removeHTMLFromContent(item.description)}</div>
+            </div>
+            {/* <Link to={`/playlist/${item.id}`}>
+            </Link> */}
+        </HoverableCard>
+    )
+}
+
+export default PlaylistCard
