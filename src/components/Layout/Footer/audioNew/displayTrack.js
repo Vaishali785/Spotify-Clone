be9b080507f5997e3707b85ecdbb0546b84ea8c6
Vaishali <aggarwal.vaishali785@gmail.com>
2024-08-27 12:44:43 +0530

    fix: Laoding in footer display

--- src/components/Layout/Footer/audioNew/displayTrack.js
+++ src/components/Layout/Footer/audioNew/displayTrack.js
@@ -1,31 +1,53 @@
-import React from 'react'
-import track from './css/DisplayTrack.module.css';
-import audio from './css/AudioPlayer.module.css';
-import { useSelector } from 'react-redux';
-
+import React from "react"
+import track from "./css/DisplayTrack.module.css"
+import audio from "./css/AudioPlayer.module.css"
+import { useSelector } from "react-redux"
 
 const DisplayTrack = () => {
-    const currentTrack = useSelector(state => state.song.songDetail);
-    return (
-        <div className={`${track['audio-slider']} ${audio['display']} `}>
-            {
-                currentTrack ?
-                    <>
-                        <div className={track["audio-info"]}>
-                            <div className={track["thumb"]}>
-                                <img src={currentTrack.album?.images[2].url} alt="Song Track " />
-                            </div>
-                            <div className={track["audio-text"]}>
-                                <div className={`${track["audio-name"]} truncate-name`}>{currentTrack?.name}</div>
-                                <div className={track["audio-singer"]}>{currentTrack && currentTrack?.artists[0].name}</div>
-                            </div>
-                        </div>
-                    </>
-                    :
-                    <p>Loading....</p>
-            }
-        </div>
-    )
+	const currentTrack = useSelector((state) => state.song.songDetail)
+	return (
+		<div className={`${track["audio-slider"]} ${audio["display"]} `}>
+			{
+				currentTrack ? (
+					<>
+						<div className={track["audio-info"]}>
+							<div className={track["thumb"]}>
+								<img
+									src={currentTrack.album?.images[2].url}
+									alt="Song Track "
+								/>
+							</div>
+							<div className={track["audio-text"]}>
+								<div className={`${track["audio-name"]} truncate-name`}>
+									{currentTrack?.name}
+								</div>
+								<div className={track["audio-singer"]}>
+									{currentTrack && currentTrack?.artists[0].name}
+								</div>
+							</div>
+						</div>
+					</>
+				) : (
+					<>
+						<div className={`${track["skeleton_audio-info"]}`}>
+							<div className={`${track["skeleton_thumb"]} skeleton-bg`}>
+								{/* <div /> */}
+							</div>
+							<div className={`${track["skeleton_audio-text"]}`}>
+								<div
+									className={`${track["skeleton_audio-name"]} skeleton-bg`}
+								></div>
+								<div
+									className={`${track["skeleton_audio-singer"]} skeleton-bg`}
+								></div>
+							</div>
+						</div>
+					</>
+				)
+				// <p>Loading....</p>
+			}
+		</div>
+	)
 }
 
 export default DisplayTrack
